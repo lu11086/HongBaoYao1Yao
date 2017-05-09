18b9083f5a986c6018f5344631eaad9ce4028b49
lu11086 <lu11086@qq.com>
2017-05-09 10:52:01 +0800

    修复bug

--- js/yao1yao.js
+++ js/yao1yao.js
@@ -24,38 +24,38 @@ imgLoader(['img/background.jpg',  'img/9.12-9.14日每天上午10点.png', 'img/
                 document.getElementById("yaoyiyaoHand").setAttribute('class', 'animated tada');
             }
         },1000);
-        //获取加速度信息
-        //通过监听上一步获取到的x, y, z 值在一定时间范围内的变化率，进行设备是否有进行晃动的判断。
-        //而为了防止正常移动的误判，需要给该变化率设置一个合适的临界值。
-        var SHAKE_THRESHOLD = 4000;
-        var last_update = 0;
-        var x, y, z, last_x = 0, last_y = 0, last_z = 0;
-
-        function deviceMotionHandler(eventData) {
-            var acceleration = eventData.accelerationIncludingGravity;
-            var curTime = new Date().getTime();
-            if ((curTime - last_update) > 10) {
-                var diffTime = curTime - last_update;
-                last_update = curTime;
-                x = acceleration.x;
-                y = acceleration.y;
-                z = acceleration.z;
-                var speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
-                if (speed > SHAKE_THRESHOLD) {
-                    clearInterval(waitYao);
-                    navigator.vibrate(1000);
-                    document.querySelector(".myCover").setAttribute('class', 'myCover animated fadeOut');
-                    getTheAward();
-                    StartMyPage();
-                }
-                last_x = x;
-                last_y = y;
-                last_z = z;
-            }
-        }
-
     }
 });
+
+//获取加速度信息
+//通过监听上一步获取到的x, y, z 值在一定时间范围内的变化率，进行设备是否有进行晃动的判断。
+//而为了防止正常移动的误判，需要给该变化率设置一个合适的临界值。
+var SHAKE_THRESHOLD = 4000;
+var last_update = 0;
+var x, y, z, last_x = 0, last_y = 0, last_z = 0;
+
+function deviceMotionHandler(eventData) {
+    if(waitYao) {clearInterval(waitYao);}
+    var acceleration = eventData.accelerationIncludingGravity;
+    var curTime = new Date().getTime();
+    if ((curTime - last_update) > 10) {
+        var diffTime = curTime - last_update;
+        last_update = curTime;
+        x = acceleration.x;
+        y = acceleration.y;
+        z = acceleration.z;
+        var speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+        if (speed > SHAKE_THRESHOLD) {
+            navigator.vibrate(1000);
+            document.querySelector(".myCover").setAttribute('class', 'myCover animated fadeOut');
+            getTheAward();
+            StartMyPage();
+        }
+        last_x = x;
+        last_y = y;
+        last_z = z;
+    }
+}
 
 function StartMyPage() {
     setTimeout(function () {
